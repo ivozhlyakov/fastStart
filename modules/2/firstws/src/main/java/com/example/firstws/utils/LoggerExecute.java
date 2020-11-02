87b5580478bed326141765cd1ec2256d241ac4ef
ivozhlyakov <ivozhlyakov@diasoft.ru>
2020-11-02 21:42:37 +0300

    2 module completed

---
+++ modules/2/firstws/src/main/java/com/example/firstws/utils/LoggerExecute.java
@@ -0,0 +1,4 @@
+package com.example.firstws.utils;
+
+public @interface LoggerExecute {
+}
---
+++ modules/2/firstws/src/test/java/com/example/firstws/FirstwsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.firstws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstwsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
